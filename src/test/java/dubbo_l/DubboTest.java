807ee388fecda3850c9f3671a8e7106962d9cf4b
yongheng.wang <yongheng@yongheng-PC>
2016-11-30 16:26:19 +0800

    dubbo消费方

--- src/test/java/dubbo_l/DubboTest.java
+++ src/test/java/dubbo_l/DubboTest.java
@@ -1,8 +1,11 @@
 package dubbo_l;
 
+import org.apache.log4j.Logger;
+
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -12,13 +15,17 @@ import com.dubbo.test.DubboProviderTestService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/spring-context.xml" })
 public class DubboTest {
+	/**
+	* Logger for this class
+	*/
+	private static final Logger logger = Logger.getLogger(DubboTest.class);
 
-	@Autowired
+	@Resource(name="dubbo.dubboProviderTestService")
 	private DubboProviderTestService dubboProviderTestService;
 	
 	@Test
 	public void testProvider(){
-		System.out.println("===测试dubbo==="+dubboProviderTestService.getHelloWord());
+		logger.info("===测试dubbo==="+dubboProviderTestService.getHelloWord());
 	}
 	
 }
